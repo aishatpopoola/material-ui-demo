45360925cc025c2e70b05b1c2b2e4c98a9a02be2
aishatpopoola <78761789+aishatpopoola@users.noreply.github.com>
2021-12-23 18:00:05 +0100

    fix: fix linter issues

--- src/components/ChangePasswordModal.js
+++ src/components/ChangePasswordModal.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { withStyles, withTheme } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
